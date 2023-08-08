372e9c7159102f8121cd10e5c5d2b132462e2592
Semiha Beyazkilic <seozcan@student.42.fr>
2023-08-08 19:25:24 +0200

    MOD: Reformating string constructor output operator

--- server/inc/Request.hpp
+++ server/inc/Request.hpp
@@ -6,7 +6,7 @@
 /*   By: seozcan <seozcan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 17:07:53 by seozcan           #+#    #+#             */
-/*   Updated: 2023/08/06 19:05:12 by seozcan          ###   ########.fr       */
+/*   Updated: 2023/08/08 18:48:00 by seozcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,32 +25,35 @@ class Request {
 
 		Request(void);
 		Request(Request const &src);
+		Request(std::string const);
 		~Request(void);
 
 		Request &	operator=(Request const &rhs);
 
 		void	setMethod(const std::string);
-		void	setPath(const std::string);
-		void	setHTTP(const std::string);
+		void	setTarget(const std::string);
+		void	setVersion(const std::string);
 		void	setQuery(const std::string);
 		void	setBody(const std::vector<char>);
-		void	setHeader(const std::map<std::string, std::vector<std::string> >);
+		void	setHeaders(const std::map<std::string, std::vector<std::string> >);
 
 		const std::string										getMethod(void) const;
-		const std::string										getPath(void) const;
-		const std::string										getHTTP(void) const;
+		const std::string										getTarget(void) const;
+		const std::string										getVersion(void) const;
 		const std::string										getQuery(void) const;
 		const std::vector<char>									getBody(void) const;
-		const std::map<std::string, std::vector<std::string> >	getHeader(void) const;
+		const std::map<std::string, std::vector<std::string> >	getHeaders(void) const;
+
+		void	parse(std::string const);
 
 	private:
 	
 		std::string												_method;
-		std::string												_path;
-		std::string												_httpVersion;
+		std::string												_target;
+		std::string												_version;
 		std::string												_query;
 		std::vector<char>										_body;
-		std::map<std::string, std::vector<std::string> >		_header;
+		std::map<std::string, std::vector<std::string> >		_headers;
 
 };
 
--- server/srcs/Request.cpp
+++ server/srcs/Request.cpp
@@ -6,7 +6,7 @@
 /*   By: seozcan <seozcan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 18:48:05 by seozcan           #+#    #+#             */
-/*   Updated: 2023/08/06 19:04:31 by seozcan          ###   ########.fr       */
+/*   Updated: 2023/08/08 19:22:47 by seozcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,17 @@
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::: CONSTRUCTORS::
 
-Request::Request(void) : _method(), _path(), _httpVersion(), _query(), _body(), _header() { };
+Request::Request(void) : _method(), _target(), _version(), _query(), _body(), _headers() { }
 
 Request::Request(Request const &src) {
 
 	if (this != &src)
 		*this = src;		
+}
+
+Request::Request(std::string const str) {
+
+	this->parse(str);
 }
 
 Request::~Request() { };
@@ -31,11 +36,11 @@ Request &	    Request::operator=(Request const & rhs) {
 	if (this != &rhs) {
 		
 		this->setMethod(rhs.getMethod());
-		this->setPath(rhs.getPath());
-		this->setHTTP(rhs.getHTTP());
+		this->setTarget(rhs.getTarget());
+		this->setVersion(rhs.getVersion());
 		this->setQuery(rhs.getQuery());
 		this->setBody(rhs.getBody());
-		this->setHeader(rhs.getHeader());
+		this->setHeaders(rhs.getHeaders());
 	}
 	
 	return *this;
@@ -45,32 +50,46 @@ Request &	    Request::operator=(Request const & rhs) {
 
 void	Request::setMethod(const std::string method) { this->_method = method; }
 
-void	Request::setPath(const std::string path) { this->_path = path; }
+void	Request::setTarget(const std::string target) { this->_target = target; }
 
-void	Request::setHTTP(const std::string httpVersion) { this->_httpVersion = httpVersion; }
+void	Request::setVersion(const std::string version) { this->_version = version; }
 
 void	Request::setQuery(const std::string query) { this->_query = query; }
 
 void	Request::setBody(const std::vector<char> body) { this->_body = body; }
 
-void	Request::setHeader(const std::map<std::string, std::vector<std::string> > header) { this->_header = header; }
+void	Request::setHeaders(const std::map<std::string, std::vector<std::string> > headers) { this->_headers = headers; }
 
-const std::string										Request::getMethod(void) const { return this->_method; }
 
-const std::string										Request::getPath(void) const { return this->_path; }
+const std::string	Request::getMethod(void) const { return this->_method; }
 
-const std::string										Request::getHTTP(void) const { return this->_httpVersion; }
+const std::string	Request::getTarget(void) const { return this->_target; }
 
-const std::string										Request::getQuery(void) const { return this->_query; }
+const std::string	Request::getVersion(void) const { return this->_version; }
+
+const std::string	Request::getQuery(void) const { return this->_query; }
 
 const std::vector<char>									Request::getBody(void) const { return this->_body; }
 
-const std::map<std::string, std::vector<std::string> >	Request::getHeader(void) const { return this->_header; }
+const std::map<std::string, std::vector<std::string> >	Request::getHeaders(void) const { return this->_headers; }
+
+// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::: MEMBER FUNCTIONS::
+
+void			Request::parse(std::string const str) {
+	
+	(void)str;
+}
 
 // ::::::::::::::::::::::::::::::::::::::::::::::: OUTPUT OPERATOR OVERLOADING::
 
 std::ostream &	operator<<(std::ostream & o, Request const & r) {
 	
-	(void)r;
-	return o << "hello Request" << std::endl;
+	o << r.getMethod() << std::endl;
+	o << r.getTarget() << std::endl;
+	o << r.getVersion() << std::endl;
+	o << r.getQuery() << std::endl;
+//	o << r.getBody() << std::endl;
+//	o << r.getHeaders() << std::endl;
+	
+	return o;
 };
