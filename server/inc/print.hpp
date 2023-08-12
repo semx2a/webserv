da5fe9b1bbef258a964448b590a7f924f4f6dbcd
Semiha Beyazkilic <seozcan@student.42.fr>
2023-08-12 15:37:43 +0200

    MOD: avoiding conflicts is they key to success

--- server/inc/print.hpp
+++ server/inc/print.hpp
@@ -6,15 +6,18 @@
 /*   By: seozcan <seozcan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/09 17:47:12 by seozcan           #+#    #+#             */
-/*   Updated: 2023/08/10 15:59:10 by seozcan          ###   ########.fr       */
+/*   Updated: 2023/08/12 15:34:51 by seozcan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PRINT_HPP
 # define PRINT_HPP
 
-#include "display.hpp"
-#include "extern_libs.hpp"
+# include <vector>
+# include <map>
+# include <iostream>
+
+# include "display.hpp"
 
 template <typename T>
 void printVector(const std::vector<T>& vec);
