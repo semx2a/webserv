89cb245273d83cd8b576152b41ef6a1f78ea154f
Semiha Beyazkilic <semiha.beyazkilic@gmail.com>
2023-08-14 09:20:32 +0200

    MOD: Header display

--- server/srcs/display.cpp
+++ server/srcs/display.cpp
@@ -1,24 +1,52 @@
 #include "../inc/display.hpp"
+#include <cstring>
+
+void	printLine(size_t size, std::string c) {
+	
+	std::cout << "  ";
+	for (size_t x = 0; x < size; x++) {
+		std::cout << c;
+	}
+	std::cout << std::endl;
+	
+}
 
 void	log(int client_fd, std::string str)
 {
-	char	date[100];
-	time_t	now;
-	tm		*brk;
-	int 	len = 68 - str.length();
+	char				date[100];
+	std::stringstream	sstr;
+	std::string			date_str;
+	std::string			header_str;
+	time_t				now;
+	tm					*brk;
 
 	time(&now);
 	brk = localtime(&now);
 	strftime(date, 99, "%B %d %Y %T", brk);
+	date_str.append(ORANGE);
+	date_str.append(date);
 
-	if (len < 0)
-		len = 0;
-	std::cout << LINE 
-			<< " | " << ORANGE << date << NO_COLOR << std::string (32, ' ') 
-				<< CYAN << "Client fd: " << client_fd << NO_COLOR << " |\n |"
-			<< PURPLE << " " << str <<  NO_COLOR << std::string(len, ' ') << '|' 
-			<< ENDLINE;
+	sstr << client_fd;
+	header_str.append(CYAN);
+	header_str.append("Client fd: ");
+	header_str.append(sstr.str());
+	header_str.append(NO_COLOR);
+
+	str.insert(0, PURPLE);
+	str.append(NO_COLOR);
+	
+	size_t	total_len = 70;
+	size_t	len_between = total_len - ((header_str.length() - strlen(ORANGE)) + (date_str.length() - (strlen(CYAN) + strlen(NO_COLOR))));
+	size_t	len = total_len - (str.length() - (strlen(PURPLE) + strlen(NO_COLOR)));
+	
+	printLine(total_len , "_");
+
+	std::cout << " |" << date_str << std::string(len_between, ' ') << header_str << "|\n";
+	std::cout << " |" << str << std::string(len, ' ') << "|\n";
+	
+	printLine(total_len, "‾");
 }
+
 
 std::string custom_width (int width, char c, std::string const&  content) {
 
