de62b0a6fd81b53e35fe9804f6ba02359a853af3
no3 <meow@no3arch>
2023-08-18 17:40:28 +0200

    MOD:prototype for a config parser

--- server/inc/Config.hpp
+++ server/inc/Config.hpp
@@ -1,6 +1,9 @@
 #ifndef CONFIG_HPP
 # define CONFIG_HPP
 
+#include <fstream>
+#include <map>
+#include <sstream>
 #include <string>
 #include <vector>
 
@@ -13,10 +16,18 @@ class Config {
 		Config& operator=(Config const& rhs);
 		~Config();
 
-		std::vector<int>&	getPorts();
+		std::vector<int>&			getPorts();
+		std::string	const&			get(const std::string& key) const; 
+		std::vector<std::string>&	getArray(const std::string& key) const; 
+
+	    Config* getBlock(const std::string& key) const;
+
 
 	private:
-		std::vector<int>	ports;
+		std::vector<int>								ports;
+		std::map<std::string, std::string>				data;
+		std::map<std::string, std::vector<std::string>>	arrayData;
+		std::map<std::string, Config*>					blockData;
 		
 };
 
--- server/srcs/Config.cpp
+++ server/srcs/Config.cpp
@@ -36,4 +36,51 @@ Config::~Config() {
 
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::GETTERS / SETTERS
 
-std::vector<int>&	Config::getPorts() { return ports; }
+std::vector<int>&			Config::getPorts() { return ports; }
+
+std::string const&			Config::get(std::string const& key) const { return data.at(key); }
+
+std::vector<std::string>&	Config::getArray(std::string const& key) const { return arrayData.at(key); }
+
+Config*						Config::getBlock(std::string const& key) const { return blockData.at(key); }
+
+//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::METHODS
+
+void parse(const std::string& filename) {
+
+	std::ifstream	file(filename.c_str());
+	std::string		line;
+
+	while (std::getline(file, line)) {
+
+		std::istringstream	is_line(line);
+		std::string			key;
+
+		if (std::getline(is_line, key, ' ')) {
+			std::string value;
+
+			if (std::getline(is_line, value)) {
+				if (value.find('{') != std::string::npos) {
+					// This is a block, so we need to parse it recursively
+					std::string blockFilename = value.substr(0, value.find('{'));
+					blockData[key] = new Config(blockFilename);
+				} else if (value.find(',') != std::string::npos) {
+					// This is an array, so we split it into multiple values
+					std::istringstream is_value(value);
+					std::string arrayValue;
+					std::vector<std::string> values;
+					while (std::getline(is_value, arrayValue, ',')) {
+						values.push_back(arrayValue);
+					}
+					arrayData[key] = values;
+				} else {
+
+					// This is a simple key-value pair
+					data[key] = value;
+				}
+			}
+		}
+	}
+}
+};
+
