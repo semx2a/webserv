9c23a8b2625f59dd1c2f421ef45a04f2d830cc72
miaou33 <nfauconn@student.42.fr>
2023-08-19 16:55:09 +0200

    MOD:return to original Config.cpp /.hpp

--- server/srcs/Config.cpp
+++ server/srcs/Config.cpp
@@ -1,52 +1,24 @@
-#include <fstream>
-#include <map>
-#include <sstream>
-#include <string>
-#include <vector>
-
-class Config {
-
-	public:
-		Config();
-		Config(std::string const& config_file);
-		Config(Config const&);
-		Config& operator=(Config const& rhs);
-		~Config();
-
-		std::vector<int>&			getPorts();
-		std::string	const&			get(const std::string& key) const; 
-		std::vector<std::string>&	getArray(const std::string& key) const; 
-
-	    Config* getBlock(const std::string& key) const;
-
-
-	private:
-		std::vector<int>									ports;
-		std::map<std::string, std::string>					data;
-		std::map<std::string, std::vector<std::string> >	arrayData;
-		std::map<std::string, Config*>						blockData;
-		
-};
+#include "../inc/Config.hpp"
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::CONSTRUCTORS / DESTRUCTORS
 
 Config::Config() {
 
-	ports.push_back(8080);
-	ports.push_back(8081);
-	ports.push_back(8082);
-	ports.push_back(8083);
-	ports.push_back(8084);
+	_ports.push_back(8080);
+	_ports.push_back(8081);
+	_ports.push_back(8082);
+	_ports.push_back(8083);
+	_ports.push_back(8084);
 }
 
-Config::Config(std::string const& conf_conf_file) {
+Config::Config(std::string const& conf_file) {
 
-	(void) conf_conf_file;
-	ports.push_back(8080);
-	ports.push_back(8081);
-	ports.push_back(8082);
-	ports.push_back(8083);
-	ports.push_back(8084);
+	(void) conf_file;
+	_ports.push_back(8080);
+	_ports.push_back(8081);
+	_ports.push_back(8082);
+	_ports.push_back(8083);
+	_ports.push_back(8084);
 }
 
 Config::Config(Config const& rhs) { *this = rhs; }
@@ -64,51 +36,7 @@ Config::~Config() {
 
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::GETTERS / SETTERS
 
-std::vector<int>&			Config::getPorts() { return ports; }
-
-std::string const&			Config::get(std::string const& key) const { return data.at(key); }
-
-std::vector<std::string>&	Config::getArray(std::string const& key) const { return arrayData.at(key); }
-
-Config*						Config::getBlock(std::string const& key) const { return blockData.at(key); }
+std::vector<int>&			Config::getPorts() { return _ports; }
 
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::METHODS
-
-void parse(const std::string& conf_filename) {
-
-	std::ifstream	conf_file(conf_filename.c_str());
-	std::string		line;
-
-	while (std::getline(conf_file, line)) {
-
-		std::istringstream	is_line(line);
-		std::string			key;
-
-		if (std::getline(is_line, key, ' ')) {
-			std::string value;
-
-			if (std::getline(is_line, value)) {
-				if (value.find('{') != std::string::npos) {
-					// This is a block, so we need to parse it recursively
-					std::string blockFilename = value.substr(0, value.find('{'));
-					blockData[key] = new Config(blockFilename);
-				} else if (value.find(',') != std::string::npos) {
-					// This is an array, so we split it into multiple values
-					std::istringstream is_value(value);
-					std::string arrayValue;
-					std::vector<std::string> values;
-					while (std::getline(is_value, arrayValue, ',')) {
-						values.push_back(arrayValue);
-					}
-					arrayData[key] = values;
-				} else {
-
-					// This is a simple key-value pair
-					data[key] = value;
-				}
-			}
-		}
-	}
-}
-};
 
