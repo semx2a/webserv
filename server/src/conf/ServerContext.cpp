3225dfd3b9d108cc1062e5745759cba3a33defe8
no3 <meow@no3arch>
2023-09-22 00:25:57 +0200

    [MOD] gave shorter names to some directives

--- server/src/conf/ServerContext.cpp
+++ server/src/conf/ServerContext.cpp
@@ -2,15 +2,15 @@
 
 //::::::::::::::::::::::::::::::::::::::::::::::::::::CONSTRUCTORS / DESTRUCTORS
 
-ServerContext::ServerContext() : _autoindex("off"), _clientMaxBodySize(1048576) {
+ServerContext::ServerContext() : _autoindex("off"), _maxBodySize(1048576) {
 
 //	this->_locations["/"] = "../www/html";
 //	this->_errorPages[404] = "../www/html/error/404.html";
 //	//NOTE: mettre valeurs par defaut APRES le parsing, SI le vecteur est vide (pareil pour les autres vecteurs)
 //	this->_index.push_back("index.html");
-//	this->_authorizedMethods.push_back("GET");
-//	this->_authorizedMethods.push_back("POST");
-//	this->_authorizedMethods.push_back("DELETE");
+//	this->_limitExcept.push_back("GET");
+//	this->_limitExcept.push_back("POST");
+//	this->_limitExcept.push_back("DELETE");
 //	//If the directive is not present then either *:80 is used if nginx runs with the superuser privileges, or *:8000 otherwise.
 //	this->_listen["*"] = 80;
 //	this->_serverNames.push_back("localhost");
@@ -22,13 +22,13 @@ ServerContext& ServerContext::operator=(ServerContext const& rhs) {
 
 	if (this != &rhs) {
 		
-		this->_clientMaxBodySize = rhs.getClientMaxBodySize();
+		this->_maxBodySize = rhs.getMaxBodySize();
 		this->_errorPages = rhs.getErrorPages();
 		this->_index = rhs.getIndex();
 		this->_root = rhs.getRoot();
 		this->_locations = rhs.getLocations();
 		this->_autoindex = rhs.getAutoindex();
-		this->_authorizedMethods = rhs.getAuthorizedMethods();
+		this->_limitExcept = rhs.getLimitExcept();
 		this->_listen = rhs.getListen();
 		this->_serverNames = rhs.getServerNames();
 	}
@@ -40,7 +40,7 @@ ServerContext::~ServerContext() {}
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::GETTERS
 
 bool										ServerContext::getAutoindex(void) const { return this->_autoindex; }
-size_t										ServerContext::getClientMaxBodySize(void) const { return this->_clientMaxBodySize; }
+size_t										ServerContext::getMaxBodySize(void) const { return this->_maxBodySize; }
 std::string const &							ServerContext::getRoot(void) const { return this->_root; }
 
 std::map<std::string, int> const &			ServerContext::getListen(void) const { return this->_listen; }
@@ -49,12 +49,12 @@ std::map<std::string, std::string> const &	ServerContext::getLocations(void) con
 
 std::vector<std::string> const & 			ServerContext::getIndex(void) const { return this->_index; }
 std::vector<std::string> const &			ServerContext::getServerNames(void) const { return this->_serverNames; }
-std::vector<std::string> const &			ServerContext::getAuthorizedMethods(void) const { return this->_authorizedMethods; }
+std::vector<std::string> const &			ServerContext::getLimitExcept(void) const { return this->_limitExcept; }
 
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::SETTERS
 
 void	ServerContext::setAutoindex(bool autoindex) { this->_autoindex = autoindex; }
-void	ServerContext::setClientMaxBodySize(size_t size) { this->_clientMaxBodySize = size; }
+void	ServerContext::setMaxBodySize(size_t size) { this->_maxBodySize = size; }
 void	ServerContext::setRoot(std::string const& root) { this->_root = root; }
 
 void	ServerContext::setListen(std::string const& ip, int port) { this->_listen[ip] = port; }
@@ -62,7 +62,7 @@ void	ServerContext::setErrorPage(std::map<int, std::string> const& errorPages) {
 void	ServerContext::setLocation(std::string const& location, std::string const& root) { this->_locations[location] = root; }
 
 void	ServerContext::setIndex(std::vector<std::string> const& index) { this->_index = index; }
-void	ServerContext::setAuthorizedMethods(std::vector<std::string> const& authorizedMethods) { this->_authorizedMethods = authorizedMethods; }
+void	ServerContext::setLimitExcept(std::vector<std::string> const& limitExcept) { this->_limitExcept = limitExcept; }
 void	ServerContext::setServerNames(std::vector<std::string> const& serverNames) { this->_serverNames = serverNames; }
 
 //:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::METHODS
@@ -72,13 +72,13 @@ void	ServerContext::setServerNames(std::vector<std::string> const& serverNames) 
 
 std::ostream &	operator<<(std::ostream & o, ServerContext const & sc) {
 
-	o << HIPURPLE << std::setw(21) << "clientMaxBodySize: "	<< HIGREEN << sc.getClientMaxBodySize()					<< NO_COLOR << std::endl;
+	o << HIPURPLE << std::setw(21) << "maxBodySize: "		<< HIGREEN << sc.getMaxBodySize()					<< NO_COLOR << std::endl;
 	o << HIPURPLE << std::setw(21) << "errorPages: " 		<< HIGREEN << print_map(sc.getErrorPages()) 			<< NO_COLOR << std::endl;
 	o << HIPURPLE << std::setw(21) << "index: " 			<< HIGREEN << print_vector(sc.getIndex())				<< NO_COLOR << std::endl;
 	o << HIPURPLE << std::setw(21) << "root: " 				<< HIGREEN << sc.getRoot()								<< NO_COLOR << std::endl;
 	o << HIPURPLE << std::setw(21) << "locations: " 		<< HIGREEN << print_map(sc.getLocations())				<< NO_COLOR << std::endl;
 	o << HIPURPLE << std::setw(21) << "autoindex: " 		<< HIGREEN << sc.getAutoindex()							<< NO_COLOR << std::endl;
-	o << HIPURPLE << std::setw(21) << "authorizedMethods: "	<< HIGREEN << print_vector(sc.getAuthorizedMethods()) 	<< NO_COLOR << std::endl;
+	o << HIPURPLE << std::setw(21) << "limitExcept: "		<< HIGREEN << print_vector(sc.getLimitExcept()) 	<< NO_COLOR << std::endl;
 	o << HIPURPLE << std::setw(21) << "listen: " 			<< HIGREEN << print_map(sc.getListen()) 				<< NO_COLOR << std::endl;
 	o << HIPURPLE << std::setw(21) << "serverNames: " 		<< HIGREEN << print_vector(sc.getServerNames())			<< NO_COLOR << std::endl;
 
--- server/src/conf/ServerContext.hpp
+++ server/src/conf/ServerContext.hpp
@@ -1,11 +1,7 @@
 #ifndef SERVER_CONTEXT_HPP
 # define SERVER_CONTEXT_HPP
 
-#include <fstream>
-#include <iostream>
 #include <map>
-#include <sstream>
-#include <string>
 #include <vector>
 
 #include "print.hpp"
@@ -16,10 +12,10 @@ class ServerContext {
 		ServerContext();
 		ServerContext(ServerContext const&);
 		ServerContext& operator=(ServerContext const& rhs);
-		virtual ~ServerContext();
+		~ServerContext();
 
 		bool										getAutoindex() const;
-		size_t										getClientMaxBodySize() const;
+		size_t										getMaxBodySize() const;
 		std::string const&							getRoot() const;
 
 		std::map<std::string, int> const&			getListen(void) const;
@@ -27,11 +23,11 @@ class ServerContext {
 		std::map<std::string, std::string> const&	getLocations() const;
 
 		std::vector<std::string> const&				getIndex() const;
-		std::vector<std::string> const&				getAuthorizedMethods() const;
+		std::vector<std::string> const&				getLimitExcept() const;
 		std::vector<std::string> const&				getServerNames(void) const;
 
 		void	setAutoindex(bool autoindex);
-		void	setClientMaxBodySize(size_t size);
+		void	setMaxBodySize(size_t size);
 		void	setRoot(std::string const& root);
 
 		void	setListen(std::string const& ip, int port);
@@ -39,12 +35,12 @@ class ServerContext {
 		void	setLocation(std::string const& location, std::string const& root);
 
 		void	setIndex(std::vector<std::string> const& index);
-		void	setAuthorizedMethods(std::vector<std::string> const& authorizedMethods);
+		void	setLimitExcept(std::vector<std::string> const& limitExcept);
 		void	setServerNames(std::vector<std::string> const& serverNames);
 
 	private:
 		bool								_autoindex;
-		size_t								_clientMaxBodySize;
+		size_t								_maxBodySize;
 		std::string							_root;
 
 		std::map<std::string, int> 			_listen; // <IP, port>
@@ -52,7 +48,7 @@ class ServerContext {
 		std::map<std::string, std::string>	_locations; // special : location context. Class?
 		
 		std::vector<std::string>			_index;
-		std::vector<std::string>			_authorizedMethods;
+		std::vector<std::string>			_limitExcept; // authorized methods
 		std::vector<std::string>			_serverNames;
 
 };
