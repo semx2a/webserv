2c5b7a6ed9376e58d4824c69e3629ac4df498a43
no3 <meow@no3arch>
2023-10-08 16:54:38 +0200

    [MOD] added tests GET

--- server/test/src/test_get.py
+++ server/test/src/test_get.py
@@ -10,20 +10,19 @@ import json
 BASE_URL = "http://localhost:8001"
 
 # Test GET
-#def test_get(endpoint):
-#	response = requests.get(f"{BASE_URL}{endpoint}")
-#	print(colored(f"GET {endpoint}:", 'cyan'), colored(f"{response.status_code}, {response.text}", 'green' if response.status_code == 200 else 'red'))
+def test_get(endpoint):
+	response = requests.get(f"{BASE_URL}{endpoint}")
+	print(colored(f"GET {endpoint}:", 'cyan'), colored(f"{response.status_code}, {response.text}", 'green' if response.status_code == 200 else 'red'))
 
-#def test_get_with_params(endpoint, params):
-#	response = requests.get(f"{BASE_URL}{endpoint}", params=params)
-#	print(colored(f"GET {endpoint}:", 'cyan'), colored(f"{response.status_code}, {response.text}", 'green' if response.status_code == 200 else 'red'))
+def test_get_with_params(endpoint, params):
+	response = requests.get(f"{BASE_URL}{endpoint}", params=params)
+	print(colored(f"GET {endpoint}:", 'cyan'), colored(f"{response.status_code}, {response.text}", 'green' if response.status_code == 200 else 'red'))
 
 if __name__ == "__main__":
 
 # Test d'une requête GET sur l'endpoint "/"
-#	test_get("/")
-#	test_get("/")
+	test_get("/")
 
-	# Test d'une requête GET sur l'endpoint "/data"
-#	test_get_with_params("/nyah", {"key": "value"})
-#	test_get_with_params("/", {"key": "value"})
+# Test d'une requête GET sur l'endpoint "/data"
+	test_get_with_params("/nyah", {"key": "value"})
+	test_get_with_params("/", {"key": "value"})
