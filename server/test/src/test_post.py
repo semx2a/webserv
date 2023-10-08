6d5bdf2b0da5ab0d5df0b00e5a1d852b6678be19
no3 <meow@no3arch>
2023-10-08 14:35:20 +0200

    [ADD] testPOST sendfile

--- server/test/src/test_post.py
+++ server/test/src/test_post.py
@@ -10,15 +10,25 @@ import json
 
 BASE_URL = "http://localhost:8001"
 
+# test POST sendfile
+def send_file(endpoint, file_path):
+    headers = {'Content-type': 'multipart/form-data'}
+    
+    with open(file_path, 'rb') as f:
+        files = {'file': f}
+        response = requests.post(f"{BASE_URL}{endpoint}", files=files, headers=headers)
+        
+        print(f"POST {endpoint}: {response.status_code}, {response.text}")
 
-# Test POST
-def test_post(endpoint, data):
-	headers = {'Content-type': 'application/python'}
-	response = requests.post(f"{BASE_URL}{endpoint}", data=data, headers=headers)
-	print(colored(f"POST {endpoint}:", 'cyan'), colored(f"{response.status_code}, {response.text}", 'green' if response.status_code == 200 else 'red'))
+## Test POST
+#def test_post(endpoint, data):
+#	headers = {'Content-type': 'application/python'}
+#	response = requests.post(f"{BASE_URL}{endpoint}", data=data, headers=headers)
+#	print(colored(f"POST {endpoint}:", 'cyan'), colored(f"{response.status_code}, {response.text}", 'green' if response.status_code == 200 else 'red'))
 
 if __name__ == "__main__":
 
-	# Test d'une requête POST sur l'endpoint "/data" avec un objet JSON comme donnée
-	test_post("/data", {"key": "value"})
+    send_file("/data", "posttest.txt")
+#	# Test d'une requête POST sur l'endpoint "/data" avec un objet JSON comme donnée
+#	test_post("/data", {"key": "value"})
 
