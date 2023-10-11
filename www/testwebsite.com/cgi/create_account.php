86d5365ac00ba668c61fe34f49157de1d10faa5c
Semiha Beyazkilic <seozcan@student.42.fr>
2023-10-11 18:24:21 +0200

    [MOD] testwebsite conf

--- www/testwebsite.com/cgi/create_account.php
+++ www/testwebsite.com/cgi/create_account.php
@@ -1,30 +1,33 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    
-    // Path to user data file
-    $userDataFilePath = "/var/www/testwebsite.com/data/users/$username.txt";
-    
-    // Check if the user already exists
-    if (file_exists($userDataFilePath)) {
-        echo "Username already exists. Please choose a different username.";
-    } else {
-        // Hash the password securely before storing it
-        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-    
-        // Create user data in JSON format
-        $userData = [
-            "username" => $username,
-            "password" => $hashedPassword,
-            "email" => $email,
-            "fullname" => $fullname
-        ];
-    
-        // Save user data to the file
-        file_put_contents($userDataFilePath, json_encode($userData));
-    
-        echo "Account created successfully!";
-    $username = $_POST["username"];
-    $password = $_POST["password"];
-    }
+
+// Get user input (username, password, email, fullname)
+$username = $_POST["username"];
+$password = $_POST["password"];
+$email = $_POST["email"];
+$fullname = $_POST["fullname"];
+
+// Path to user data file
+$userDataFilePath = "/var/www/testwebsite.com/data/users/$username.txt";
+
+if (!file_exists($userDataFilePath)) {
+    // Hash the password securely before storing it
+    $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+
+    // Create user data in JSON format
+    $userData = [
+        "username" => $username,
+        "password" => $hashedPassword,
+        "email" => $email,
+        "fullname" => $fullname
+    ];
+
+    // Save user data to the file
+    file_put_contents($userDataFilePath, json_encode($userData));
+
+    // Display success message
+    echo "Account successfully created";
+} else {
+    // Account already exists
+    echo "Account with username '$username' already exists. Please choose a different username.";
 }
 ?>
